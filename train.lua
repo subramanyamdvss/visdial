d2d7df3fbb0564130e63cf9e35eb4726765792a6
subramanyamdvss <subramanyamdvss@gmail.com>
2017-06-03 10:25:02 +0530

    Update train.lua

--- train.lua
+++ train.lua
@@ -63,6 +63,55 @@ local model = Model(modelParams);
 ------------------------------------------------------------------------
 -- Training
 ------------------------------------------------------------------------
+
+function topk(list, k, comp)
+    collectgarbage()
+    cutorch.synchronize()
+    time = sys.clock()
+    local subset = (list.data)
+    print(torch.type(subset))
+    torch.setdefaulttensortype('torch.CudaTensor' )
+    for i = 1,(list:size()),(opt.batchSize*40) do
+        xlua.progress(i,list:size())
+        local btch = subset:sub(i,math.min(i+opt.batchSize*40-1,list:size()))
+        if i == 1 then
+            logprob = net:forward(btch)
+        else
+        logprob = torch.cat({logprob,net:forward(btch)},1)
+        end
+    end
+    cutorch.synchronize()
+    print('\ntime taken',sys.clock()-time)
+    time = sys.clock()
+    collectgarbage()
+    subset = -((torch.cmul(logprob,torch.exp(logprob))*(torch.Tensor(#classes,1):fill(1))):t())
+    print(torch.type(subset))
+    y , list2 = torch.sort(subset,2,true)
+    cutorch.synchronize()
+    print('time taken',sys.clock()-time)
+    time = sys.clock()
+    print(torch.type(list2),torch.type(list.data),torch.type(list.label))
+    list.data:indexCopy(1,list2[1],(list.data))
+    list.label:indexCopy(1,list2[1],(list.label))
+    cutorch.synchronize()
+    print('time taken',sys.clock()-time)
+    time = sys.clock()
+    list2:free()
+    batch = list.data:sub(1,k)
+    labels = list.label:sub(1,k)
+    collectgarbage()
+    list.data = list.data:sub(k+1,(#(list.data))[1]) 
+    collectgarbage()
+    list.label = list.label:sub(k+1,(#(list.label))[1]) 
+    collectgarbage()
+    print('dvss')
+    cutorch.synchronize()
+    print('time taken',sys.clock()-time)
+    time = sys.clock()
+    return batch,labels,list;
+end 
+
+
 print('Training..')
 collectgarbage()
 
